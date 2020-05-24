6228058b8b5e2a0354f74407e2ea134c4b3ac2db
sonymnidhiry <52722948+sonymnidhiry@users.noreply.github.com>
2020-05-24 19:19:38 -0400

    first commit

---
+++ CareerCloud.Pocos/SecurityRolePoco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Security_Roles")]
+    public class SecurityRolePoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Role { get; set; }
+        [Column("Is_Inactive")]
+        public Boolean IsInactive { get; set; }
+
+        public virtual ICollection<SecurityLoginsRolePoco> SecurityLoginsRoles { get; set; }
+    }
+}
